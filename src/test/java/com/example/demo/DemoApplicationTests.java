48e285cfd745b7ff84cd760c4dbac2105fdc8f92
Savina Daria <savina.dariia@chnu.edu.ua>
2024-06-07 04:02:59 +0300

    6.4

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,16 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("spring-boot-test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
