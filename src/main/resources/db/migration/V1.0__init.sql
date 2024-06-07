f21585a591f98b59023d5d9aa2ba2dd6dfbb7cec
Savina Daria <savina.dariia@chnu.edu.ua>
2024-06-07 03:36:36 +0300

    6.2

---
+++ src/main/resources/db/migration/V1.0__init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE tt_team_member
+(
+    id UUID NOT NULL,
+    PRIMARY KEY (id)
+);
--- src/test/java/com/example/demo/StaffMemberRepositoryTest.java
+++ src/test/java/com/example/demo/StaffMemberRepositoryTest.java
@@ -1,19 +1,27 @@
 package com.example.demo;
+import io.github.wimdeblauwe.jpearl.InMemoryUniqueIdGenerator;
+import io.github.wimdeblauwe.jpearl.UniqueIdGenerator;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.JdbcTemplate;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.test.context.ActiveProfiles;
 
 import com.example.demo.staff.*;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
+@ActiveProfiles("data-jpa-test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class StaffMemberRepositoryTest {
     private final StaffMemberRepository repository;
     private final JdbcTemplate jdbcTemplate;
@@ -22,7 +30,7 @@ class StaffMemberRepositoryTest {
 
     @Autowired
     StaffMemberRepositoryTest(StaffMemberRepository repository,
-                           JdbcTemplate jdbcTemplate) {
+                             JdbcTemplate jdbcTemplate) {
         this.repository = repository;
         this.jdbcTemplate = jdbcTemplate;
     }
@@ -39,7 +47,16 @@ class StaffMemberRepositoryTest {
 
         entityManager.flush();
 
-        UUID idInDb = jdbcTemplate.queryForObject("SELECT id FROM Staffmember", UUID.class);
+        UUID idInDb = jdbcTemplate.queryForObject("SELECT id FROM tt_Staff_member", UUID.class);
         assertThat(idInDb).isEqualTo(id.getId());
+    }
+
+    @TestConfiguration
+    static class TestConfig {
+
+        @Bean
+        public UniqueIdGenerator<UUID> uniqueIdGenerator() {
+            return new InMemoryUniqueIdGenerator();
+        }
     }
 }
